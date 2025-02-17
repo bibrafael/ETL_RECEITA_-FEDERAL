2730050a584d96a976f72585cd913e7959855c15
Rafael.Souza <bibrafael@gmail.com>
2025-02-17 10:59:12 -0300

    Transformando o cnae em arquivo parquet

--- tratam_cnae.py
+++ tratam_cnae.py
@@ -1,24 +1,57 @@
 import pandas as pd
+import glob, os
+from tqdm import tqdm
+import pyarrow.parquet as pq
+import pyarrow as pa
 
-caminho_arquivo = "arquivos_extraidos/CNAE/F.K03200$Z.D50111.CNAECSV"
-novo_arquivo = "arquivos_extraidos/CNAE/CNAE_FORMATADO.csv"  # Nome do novo arquivo
+# Caminho da pasta onde estão os arquivos CSV
+pasta_cnae = "arquivos_extraidos/CNAE/"
 
-# Lendo o arquivo sem cabeçalho
-df = pd.read_csv(
-    caminho_arquivo, 
-    sep=";", 
-    encoding="latin1",  
-    dtype=str, 
-    quotechar='"', 
-    header=None  # Indica que não há cabeçalho na primeira linha
-)
+# Usar glob para listar todos os arquivos CSV dentro da pasta
+arquivo_cnae = glob.glob(os.path.join(pasta_cnae, "*.csv"))
 
-# Definindo manualmente os nomes das colunas
-df.columns = ["CÓDIGO", "DESCRIÇÃO"]
+# Lista para armazenar os DataFrames de cada arquivo
+lista_dfs = []
 
-df["CÓDIGO"] = df["CÓDIGO"].str.replace(",", ".").astype(int)
+# Quantidade de linhas processadas por vez, para não estourar a memória
+chunk_size = 30000
 
-# Salvando em um novo CSV com os novos cabeçalhos
-df.to_csv(novo_arquivo, sep=";", index=False, encoding="latin1")
+# Definir o nome do arquivo parquet
+output_parquet = "cnae_completo.parquet"
 
-print(df.info())
+# Definir o caminho completo do arquivo parquet
+caminho_completo = pasta_cnae+output_parquet
+
+# Preparar o ParquetWriter para salvar os dados progressivamente
+with tqdm(total=len(arquivo_cnae), desc="Processando arquivos", unit="arquivo") as pbar:
+    for arquivo in arquivo_cnae:
+        print(f"Lendo arquivo {arquivo}")
+
+        # Processar arquivo em chunks
+        for chunk in pd.read_csv(
+                            arquivo, 
+                            sep=";", 
+                            encoding="latin1",  
+                            dtype=str, 
+                            quotechar='"', 
+                            header=None,  # Indica que não há cabeçalho na primeira linha
+                            chunksize=chunk_size
+                            ):
+            
+            # Definindo manualmente os nomes das colunas
+            chunk.columns = ["CÓDIGO", "DESCRIÇÃO"]
+            
+            # Remover espaços extras das colunas
+            chunk = chunk.map(lambda x: x.strip() if isinstance(x, str) else x)
+
+            # Converter o DataFrame em um TAble (para o formato Parquet)
+            tabela_cnae = pa.Table.from_pandas(chunk)
+
+            # Usar ParquetWriter para salvar os dados progressivamente
+            if not os.path.exists(output_parquet): # Se o arquivo não existe, cria o arquivo
+                pq.write_table(tabela_cnae, caminho_completo, compression='SNAPPY')
+            else: # Caso contrário, abre o arquivo e anexa os dados
+                with pq.ParquetWriter(caminho_completo, tabela_cnae.schema, compression='SNAPPY') as writer:
+                    writer.write_table(tabela_cnae)
+                    
+print("Arquivo Parquet salvo com sucesso!")
